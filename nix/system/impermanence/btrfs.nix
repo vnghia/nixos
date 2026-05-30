0a48c301fa2b6f3e32c7f05491576b9f58e05496
Vo Van Nghia <git@vnghia.com>
2026-05-30 23:11:54 +0200

    fix(nix/system): use boot systemd initrd for brtfs impermanence

--- nix/system/impermanence/btrfs.nix
+++ nix/system/impermanence/btrfs.nix
@@ -3,36 +3,48 @@
   ...
 }:
 {
-  boot.initrd.postResumeCommands = lib.mkAfter ''
-    mkdir -p /btrfs_root
+  boot.initrd.systemd.services.btrfs-impermanence = {
+    description = "Manage btrfs subvolumes impermanence";
+    wantedBy = [ "initrd.target" ];
+    # make sure it's done after encryption
+    # i.e. LUKS/TPM process
+    after = [ "systemd-cryptsetup@enc.service" ];
+    # mount the root fs before clearing
+    before = [ "sysroot.mount" ];
+    unitConfig.DefaultDependencies = "no";
+    serviceConfig.Type = "oneshot";
+    script = ''
+      mkdir -p /btrfs_root
 
-    # We first mount the btrfs root to /btrfs_root
-    # so we can manipulate btrfs subvolumes.
-    mount -o subvol=/ /dev/disk/by-label/nixos /btrfs_root
+      # We first mount the btrfs root to /btrfs_root
+      # so we can manipulate btrfs subvolumes.
+      mount -o /dev/disk/by-label/nixos /btrfs_root
 
-    # We then take a snapshot of the current root
-    # before recreating a blank root.
-    mkdir -p /btrfs_root/@old_roots
-    timestamp=$(date --date="@$(stat -c %Y /btrfs_root/@root)" "+%Y-%m-%-d_%H:%M:%S")
-    btrfs subvolume snapshot -r /btrfs_root/@root "/btrfs_root/@old_roots/$timestamp"
+      # We then take a snapshot of the current root
+      # before recreating a blank root.
+      mkdir -p /btrfs_root/@old_roots
+      timestamp=$(date --date="@$(stat -c %Y /btrfs_root/@root)" "+%Y-%m-%-d_%H:%M:%S")
+      btrfs subvolume snapshot -r /btrfs_root/@root "/btrfs_root/@old_roots/$timestamp"
 
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_root/$i"
-        done
-        echo "deleting /$1 subvolume..."
-        btrfs subvolume delete "$1"
-    }
+      delete_subvolume_recursively() {
+          IFS=$'\n'
+          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+              delete_subvolume_recursively "/btrfs_root/$i"
+          done
+          echo "deleting /$1 subvolume..."
+          btrfs subvolume delete "$1"
+      }
 
-    for i in $(find /btrfs_root/@old_roots/ -maxdepth 1 -mtime +30); do
-        delete_subvolume_recursively "$i"
-    done
+      for i in $(find /btrfs_root/@old_roots/ -maxdepth 1 -mtime +30); do
+          delete_subvolume_recursively "$i"
+      done
 
-    btrfs subvolume create /btrfs_root/@root
+      delete_subvolume_recursively /btrfs_root/@root
+      btrfs subvolume create /btrfs_root/@root
 
-    # Once we're done recreating a blank root.
-    # we can unmount /btrfs_root and continue on the boot process.
-    umount /btrfs_root
-  '';
+      # Once we're done recreating a blank root.
+      # we can unmount /btrfs_root and continue on the boot process.
+      umount /btrfs_root
+    '';
+  };
 }
