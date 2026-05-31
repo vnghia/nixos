01d59f9cbc8e24dabf8977292829d4d58d4f1be3
Vo Van Nghia <git@vnghia.com>
2026-05-31 19:35:56 +0200

    fix(system/nixos/impermanence): merge btrfs

--- modules/system/nixos/impermanence/default.nix
+++ modules/system/nixos/impermanence/default.nix
@@ -7,8 +7,6 @@ let
   cfg = config.impermanence;
 in
 {
-  imports = [ ./btrfs.nix ];
-
   options = {
     impermanence = with lib; {
       enable = mkEnableOption "Impermanence";
@@ -17,41 +15,91 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    environment.persistence.${cfg.path} = {
-      hideMounts = true;
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      {
+        environment.persistence.${cfg.path} = {
+          hideMounts = true;
 
-      directories = [
-        # System state
-        "/var/log"
-        "/var/lib/nixos"
-        "/var/lib/systemd/coredump"
-        "/var/lib/systemd/timers"
-        "/var/lib/systemd/rfkill"
-        "/var/lib/systemd/backlight"
+          directories = [
+            # System state
+            "/var/log"
+            "/var/lib/nixos"
+            "/var/lib/systemd/coredump"
+            "/var/lib/systemd/timers"
+            "/var/lib/systemd/rfkill"
+            "/var/lib/systemd/backlight"
 
-        # Networking
-        "/etc/NetworkManager/system-connections"
-        "/var/lib/NetworkManager"
-        "/var/lib/tailscale"
+            # Networking
+            "/etc/NetworkManager/system-connections"
+            "/var/lib/NetworkManager"
+            "/var/lib/tailscale"
 
-        # Auth and SSH
-        "/etc/ssh"
+            # Auth and SSH
+            "/etc/ssh"
 
-        # Hardware state
-        "/var/lib/bluetooth"
-        "/var/lib/upower"
-        "/var/lib/alsa"
+            # Hardware state
+            "/var/lib/bluetooth"
+            "/var/lib/upower"
+            "/var/lib/alsa"
 
-        # Desktop session data
-        "/var/lib/AccountsService"
-        "/var/cache/cups"
-      ];
+            # Desktop session data
+            "/var/lib/AccountsService"
+            "/var/cache/cups"
+          ];
 
-      files = [
-        "/etc/machine-id"
-        "/var/lib/dbus/machine-id"
-      ];
-    };
-  };
+          files = [
+            "/etc/machine-id"
+            "/var/lib/dbus/machine-id"
+          ];
+        };
+      }
+      (lib.mkIf (cfg.type == "btrfs") {
+        boot.initrd.systemd.services.btrfs-impermanence = {
+          description = "Manage btrfs subvolumes impermanence";
+          wantedBy = [ "initrd.target" ];
+          # make sure it's done after encryption
+          # i.e. LUKS/TPM process
+          after = [ "systemd-cryptsetup@cryptroot.service" ];
+          # mount the root fs before clearing
+          before = [ "sysroot.mount" ];
+          unitConfig.DefaultDependencies = "no";
+          serviceConfig.Type = "oneshot";
+          script = ''
+            mkdir -p /btrfs
+
+            # We first mount the btrfs root to /btrfs
+            # so we can manipulate btrfs subvolumes.
+            mount -o subvol=/ /dev/mapper/cryptroot /btrfs
+
+            # We then take a snapshot of the current root
+            # before recreating a blank root.
+            mkdir -p /btrfs/@snapshots/@root
+            timestamp=$(date --date="@$(stat -c %Y /btrfs/@root)" "+%Y-%m-%-d-%H-%M-%S")
+            btrfs subvolume snapshot -r /btrfs/@root "/btrfs/@snapshots/@root/$timestamp"
+
+            delete_subvolume_recursively() {
+                IFS=$'\n'
+                for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                    delete_subvolume_recursively "/btrfs/$i"
+                done
+                echo "deleting $1 subvolume..."
+                btrfs subvolume delete "$1"
+            }
+
+            for i in $(find /btrfs/@snapshots/@root/ -maxdepth 1 -mtime +30); do
+                delete_subvolume_recursively "$i"
+            done
+
+            delete_subvolume_recursively /btrfs/@root
+            btrfs subvolume create /btrfs/@root
+
+            # Once we're done recreating a blank root.
+            # we can unmount /btrfs and continue on the boot process.
+            umount /btrfs
+          '';
+        };
+      })
+    ]
+  );
 }
