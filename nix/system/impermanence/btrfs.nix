a4d316eea16b094e89257651de6da10c4c636815
Vo Van Nghia <git@vnghia.com>
2026-05-31 10:40:25 +0200

    fix(nix/system): restruct btrfs-impermanence

--- nix/system/impermanence/btrfs.nix
+++ nix/system/impermanence/btrfs.nix
@@ -14,37 +14,37 @@
     unitConfig.DefaultDependencies = "no";
     serviceConfig.Type = "oneshot";
     script = ''
-      mkdir -p /btrfs_root
+      mkdir -p /btrfs
 
-      # We first mount the btrfs root to /btrfs_root
+      # We first mount the btrfs root to /btrfs
       # so we can manipulate btrfs subvolumes.
-      mount -o subvol=/ /dev/mapper/cryptroot /btrfs_root
+      mount -o subvol=/ /dev/mapper/cryptroot /btrfs
 
       # We then take a snapshot of the current root
       # before recreating a blank root.
-      mkdir -p /btrfs_root/@old_roots
-      timestamp=$(date --date="@$(stat -c %Y /btrfs_root/@root)" "+%Y-%m-%-d-%H-%M-%S")
-      btrfs subvolume snapshot -r /btrfs_root/@root "/btrfs_root/@old_roots/$timestamp"
+      mkdir -p /btrfs/@snapshots/@root
+      timestamp=$(date --date="@$(stat -c %Y /btrfs/@root)" "+%Y-%m-%-d-%H-%M-%S")
+      btrfs subvolume snapshot -r /btrfs/@root "/btrfs/@snapshots/@root/$timestamp"
 
       delete_subvolume_recursively() {
           IFS=$'\n'
           for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-              delete_subvolume_recursively "/btrfs_root/$i"
+              delete_subvolume_recursively "/btrfs/$i"
           done
-          echo "deleting /$1 subvolume..."
+          echo "deleting $1 subvolume..."
           btrfs subvolume delete "$1"
       }
 
-      for i in $(find /btrfs_root/@old_roots/ -maxdepth 1 -mtime +30); do
+      for i in $(find /btrfs/@snapshots/@root/ -maxdepth 1 -mtime +30); do
           delete_subvolume_recursively "$i"
       done
 
-      delete_subvolume_recursively /btrfs_root/@root
-      btrfs subvolume create /btrfs_root/@root
+      delete_subvolume_recursively /btrfs/@root
+      btrfs subvolume create /btrfs/@root
 
       # Once we're done recreating a blank root.
-      # we can unmount /btrfs_root and continue on the boot process.
-      umount /btrfs_root
+      # we can unmount /btrfs and continue on the boot process.
+      umount /btrfs
     '';
   };
 }
