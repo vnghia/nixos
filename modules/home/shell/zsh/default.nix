a7d7b9caf44fd04ebd47b2ef2fdd39e2b32593a0
Vo Van Nghia <git@vnghia.com>
2026-06-01 14:58:59 +0200

    feat(system/zsh): add plugins

--- modules/home/shell/zsh/default.nix
+++ modules/home/shell/zsh/default.nix
@@ -6,12 +6,34 @@
   ...
 }:
 let
+  cfg = config.shell.zsh;
   osCfg = osConfig.shell.zsh;
   userCfg = osConfig.user;
   xdgCfg = config.xdg;
   historyPath = "${xdgCfg.dataHome}/zsh/zsh_history";
 in
 {
+  options = with lib; {
+    shell.zsh = {
+      plugins = mkOption {
+        type = types.attrsOf (
+          types.submodule {
+            options = {
+              enable = mkOption {
+                type = lib.types.bool;
+                default = true;
+              };
+              path = mkOption {
+                type = types.nullOr types.str;
+                default = null;
+              };
+            };
+          }
+        );
+      };
+    };
+  };
+
   config = lib.mkIf (osCfg.enable || userCfg.shell == "zsh") {
     programs.zsh = {
       enable = true;
@@ -28,10 +50,32 @@ in
         saveNoDups = true;
         share = true;
       };
+
+      antidote = {
+        enable = true;
+        plugins = [
+          (lib.strings.concatLines (
+            lib.attrsets.mapAttrsToList (
+              name: value: "${name}${lib.optionalString (value.path != null) " path:${value.path}"}"
+            ) (lib.attrsets.filterAttrs (name: value: value.enable) cfg.plugins)
+          ))
+        ];
+      };
+
+      sessionVariables = {
+        HISTFILE = historyPath;
+      };
+    };
+
+    shell.zsh.plugins = {
+      "zsh-users/zsh-autosuggestions" = { };
+      "zsh-users/zsh-history-substring-search" = { };
+      "zsh-users/zsh-syntax-highlighting" = { };
     };
 
     impermanence.files = [
       (lib.strings.removePrefix "${config.home.homeDirectory}/" historyPath)
+      (lib.strings.removePrefix "${config.home.homeDirectory}/" "${xdgCfg.cacheHome}/antidote")
     ];
   };
 }
