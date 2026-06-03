f81c9b84c4e534ffe75b2c208e3c70d0a98d535f
Vo Van Nghia <git@vnghia.com>
2026-06-03 16:04:23 +0200

    feat(desktop/stylix): add base16 and sizes

--- modules/home/desktop/theming/stylix/default.nix
+++ modules/home/desktop/theming/stylix/default.nix
@@ -10,25 +10,36 @@ let
 in
 {
   config = {
-    stylix.fonts = {
-      serif = {
-        package = pkgs.ubuntu-sans;
-        name = "Ubuntu";
+    stylix = {
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/one-light.yaml";
+
+      fonts = {
+        serif = {
+          package = pkgs.ubuntu-sans;
+          name = "Ubuntu";
+        };
+        sansSerif = {
+          package = pkgs.ubuntu-sans;
+          name = "Ubuntu";
+        };
+        monospace = {
+          package = pkgs.nerd-fonts.jetbrains-mono;
+          name = "JetBrainsMono Nerd Font";
+        };
+        emoji = {
+          package = pkgs.noto-fonts-color-emoji;
+          name = "Noto Color Emoji";
+        };
+        sizes = {
+          applications = 10;
+          desktop = 10;
+          popups = 10;
+          terminal = 12;
+        };
       };
 
-      sansSerif = {
-        package = pkgs.ubuntu-sans;
-        name = "Ubuntu";
-      };
-
-      monospace = {
-        package = pkgs.nerd-fonts.jetbrains-mono;
-        name = "JetBrainsMono Nerd Font";
-      };
-
-      emoji = {
-        package = pkgs.noto-fonts-color-emoji;
-        name = "Noto Color Emoji";
+      opacity = {
+        terminal = 0.65;
       };
     };
   };
