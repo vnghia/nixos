4ab132a61d2c416d45d478e2a235ea4f6349ce36
Vo Van Nghia <git@vnghia.com>
2026-06-05 23:20:31 +0200

    feat(desktop/gnome): move extension to users module

--- modules/home/desktop/managers/gnome/default.nix
+++ modules/home/desktop/managers/gnome/default.nix
@@ -9,9 +9,8 @@
 let
   cfg = config._.desktop.managers.gnome;
   osCfg = osConfig._.desktop.managers.gnome;
-  enabledPlugins = lib.attrsets.filterAttrs (
-    name: value: value.enable && (lib.attrsets.attrByPath [ name "enable" ] true cfg.extensions)
-  ) osCfg.extensions;
+  enabledPlugins = lib.attrsets.filterAttrs (name: value: value.enable) cfg.extensions;
+
   dumpDconf =
     attrs:
     lib.attrsets.concatMapAttrs (
@@ -58,6 +57,10 @@ in
                   type = types.bool;
                   default = true;
                 };
+                key = mkOption {
+                  type = types.nullOr types.str;
+                  default = null;
+                };
                 config = mkOption {
                   type = types.nullOr (types.attrsOf types.anything);
                   default = null;
@@ -76,15 +79,229 @@ in
   config = lib.mkIf osCfg.enable (
     lib.mkMerge [
       {
+        home.packages = lib.attrsets.mapAttrsToList (
+          name: value: pkgs.gnomeExtensions.${name}
+        ) enabledPlugins;
+      }
+      {
         dconf = {
           enable = true;
-          settings = dumpDconf cfg.dconf;
+          settings = lib.mkMerge [
+            (dumpDconf cfg.dconf)
+            (dumpDconf {
+              org.gnome.shell = {
+                favorite-apps = cfg.favorites;
+
+                enabled-extensions = lib.attrsets.mapAttrsToList (
+                  name: _: pkgs.gnomeExtensions.${name}.extensionUuid
+                ) enabledPlugins;
+
+                extensions = lib.attrsets.mapAttrs' (
+                  name: value:
+                  lib.attrsets.nameValuePair (if value.key != null then value.key else name) (
+                    if value.config != null then value.config else { }
+                  )
+                ) enabledPlugins;
+              };
+            })
+            (dumpDconf {
+              org.gnome.shell.extensions = lib.attrsets.mapAttrs' (
+                name: value:
+                lib.attrsets.nameValuePair (if value.key != null then value.key else name) (
+                  lib.attrsets.attrByPath [
+                    name
+                    "config"
+                  ] { } osCfg.extensions
+                )
+              ) enabledPlugins;
+            })
+          ];
         };
       }
       (customLib.desktop.theming.stylix.mkConfig "gnome" (cfg // { enable = osCfg.stylix; }))
       {
         _ = {
           desktop.managers.gnome = {
+            extensions = {
+              appindicator = {
+                config = {
+                  legacy-tray-enabled = false;
+                  tray-pos = "right";
+                };
+              };
+              blur-my-shell = {
+                config = {
+                  hacks-level = 1;
+                  rounded-blur-found = false;
+                  settings-version = 2;
+                  appfolder = {
+                    blur = true;
+                    brightness = 1.0;
+                    sigma = 0;
+                  };
+                  applications = {
+                    pipeline = "pipeline_default";
+                  };
+                  coverflow-alt-tab = {
+                    pipeline = "pipeline_default";
+                  };
+                  dash-to-dock = {
+                    blur = true;
+                    brightness = 1.0;
+                    pipeline = "pipeline_default_rounded";
+                    sigma = 100;
+                    static-blur = true;
+                    style-dash-to-dock = 0;
+                    unblur-in-overview = false;
+                  };
+                  hidetopbar = {
+                    compatibility = false;
+                  };
+                  lockscreen = {
+                    pipeline = "pipeline_default";
+                  };
+                  overview = {
+                    blur = true;
+                    pipeline = "pipeline_default";
+                    style-components = 1;
+                  };
+                  panel = {
+                    brightness = 1.0;
+                    force-light-text = false;
+                    pipeline = "pipeline_default";
+                    sigma = 0;
+                    unblur-in-overview = true;
+                  };
+                  screenshot = {
+                    pipeline = "pipeline_default";
+                  };
+                  window-list = {
+                    brightness = 1.0;
+                    sigma = 0;
+                  };
+                };
+              };
+              caffeine = {
+                config = {
+                  cli-toggle = false;
+                  indicator-position-max = 1;
+                  user-enabled = true;
+                };
+              };
+              clipboard-indicator = {
+                config = {
+                  excluded-apps = [ "KeePassXC" ];
+                };
+              };
+              dash-to-dock = {
+                config = {
+                  always-center-icons = true;
+                  animate-show-apps = true;
+                  apply-custom-theme = true;
+                  apply-glossy-effect = false;
+                  autohide = true;
+                  background-opacity = 0.5;
+                  custom-background-color = false;
+                  custom-theme-shrink = false;
+                  dash-max-icon-size = 44;
+                  disable-overview-on-startup = true;
+                  dock-fixed = false;
+                  dock-position = "BOTTOM";
+                  extend-height = false;
+                  height-fraction = 0.9;
+                  hide-tooltip = false;
+                  icon-size-fixed = false;
+                  intellihide = false;
+                  intellihide-mode = "FOCUS_APPLICATION_WINDOWS";
+                  isolate-monitors = false;
+                  isolate-workspaces = false;
+                  max-alpha = 0.8;
+                  middle-click-action = "launch";
+                  multi-monitor = true;
+                  preview-size-scale = 0.0;
+                  running-indicator-dominant-color = false;
+                  running-indicator-style = "DOTS";
+                  scroll-action = "do-nothing";
+                  shift-click-action = "minimize";
+                  shift-middle-click-action = "launch";
+                  show-apps-always-in-the-edge = true;
+                  show-show-apps-button = false;
+                  show-trash = true;
+                  transparency-mode = "FIXED";
+                  unity-backlit-items = false;
+                };
+              };
+              disable-unredirect = { };
+              emoji-copy = {
+                config = {
+                  emoji-keybind = [ "<Super>E" ];
+                };
+              };
+              hide-top-bar = {
+                key = "hidetopbar";
+                config = {
+                  enable-active-window = false;
+                  enable-intellihide = false;
+                  mouse-sensitive = true;
+                };
+              };
+              just-perfection = {
+                config = {
+                  accessibility-menu = false;
+                  activities-button = false;
+                  background-menu = true;
+                  clock-menu = true;
+                  controls-manager-spacing-size = 0;
+                  dash = true;
+                  dash-app-running = true;
+                  dash-icon-size = 0;
+                  dash-separator = true;
+                  double-super-to-appgrid = true;
+                  keyboard-layout = true;
+                  max-displayed-search-results = 0;
+                  osd = true;
+                  overlay-key = true;
+                  panel = true;
+                  panel-in-overview = true;
+                  panel-notification-icon = true;
+                  power-icon = true;
+                  quick-settings = true;
+                  ripple-box = false;
+                  search = false;
+                  show-apps-button = false;
+                  startup-status = 0;
+                  support-notifier-showed-version = 34;
+                  support-notifier-type = 0;
+                  theme = true;
+                  type-to-search = true;
+                  window-demands-attention-focus = true;
+                  window-maximized-on-create = false;
+                  window-picker-icon = false;
+                  window-preview-caption = true;
+                  window-preview-close-button = false;
+                  workspace = true;
+                  workspace-background-corner-size = 0;
+                  workspace-peek = false;
+                  workspace-popup = true;
+                  workspace-switcher-should-show = true;
+                  workspace-thumbnail-to-main-view = false;
+                  workspace-wrap-around = true;
+                  workspaces-in-app-grid = false;
+                };
+              };
+              maximize-window-into-new-workspace = { };
+              night-theme-switcher = {
+                key = "nightthemeswitcher";
+                config = {
+                  time = {
+                    manual-schedule = false;
+                    nightthemeswitcher-ondemand-keybinding = [ "<Shift><Super>t" ];
+                  };
+                };
+              };
+              quick-settings-audio-panel = { };
+            };
+
             dconf = {
               org.gnome = lib.mkMerge [
                 {
@@ -128,10 +345,6 @@ in
 
                   shell = {
                     disable-extension-version-validation = true;
-                    enabled-extensions = lib.attrsets.mapAttrsToList (
-                      name: _: pkgs.gnomeExtensions.${name}.extensionUuid
-                    ) enabledPlugins;
-                    favorite-apps = cfg.favorites;
 
                     app-switcher = {
                       current-workspace-only = false;
@@ -144,14 +357,6 @@ in
                   tweaks = {
                     show-extensions-notice = false;
                   };
-                }
-                {
-                  shell.extensions = lib.attrsets.concatMapAttrs (name: value: {
-                    ${if value.key != null then value.key else name} = lib.mkMerge [
-                      (if value.config != null then value.config else { })
-                      (lib.attrsets.attrByPath [ name "config" ] { } cfg.extensions)
-                    ];
-                  }) enabledPlugins;
                 }
               ];
             };
