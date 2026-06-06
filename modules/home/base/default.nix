c35725f6f3b3be3d4142bd2084efdf9baabf8e39
Vo Van Nghia <git@vnghia.com>
2026-06-06 10:39:36 +0200

    fix(home): link specialisation to a safe directory

--- modules/home/base/default.nix
+++ modules/home/base/default.nix
@@ -2,11 +2,15 @@
   lib,
   config,
   osConfig,
+  pkgs,
   ...
 }:
 let
   cfg = config._.user;
   shellCfg = osConfig._.shell;
+  xdgCfg = config.xdg;
+  homeSpecialisationDirectory = "${xdgCfg.stateHome}/home-manager/gcroots/current-home/specialisation";
+  specialisationDirectory = "${xdgCfg.stateHome}/specialisation";
 in
 {
   options = with lib; {
@@ -38,6 +42,27 @@ in
     programs = {
       home-manager = {
         enable = true;
+      };
+    };
+
+    systemd.user.services.link-specialisation = {
+      Unit = {
+        Description = "Link specialisation to ${specialisationDirectory}";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.writeShellScript "link-specialisation" ''
+          mkdir -p ${specialisationDirectory}
+          for specialisation in $(find ${homeSpecialisationDirectory} -mindepth 1 -maxdepth 1 -type l); do
+            name=${specialisationDirectory}/$(basename $specialisation)
+            echo "Linking $specialisation to $name ..."
+            rm $name
+            ln -s $(realpath $specialisation) $name
+          done
+        ''}";
       };
     };
   };
