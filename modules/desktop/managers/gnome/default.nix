6dae4697bb2d25921a491a275b7bb2cbdf4786fb
Vo Van Nghia <git@vnghia.com>
2026-06-14 14:12:38 +0200

    fix(desktop/gnome): dont persist gdm lib (make login slower)

--- modules/desktop/managers/gnome/default.nix
+++ modules/desktop/managers/gnome/default.nix
@@ -52,11 +52,5 @@ in
     environment.systemPackages = [
       pkgs.gnome-terminal
     ];
-
-    _ = {
-      system.nixos.impermanence.directories = [
-        "/var/lib/gdm"
-      ];
-    };
   };
 }
