82985dda940276777a2372253dd50f081f386599
Vo Van Nghia <git@vnghia.com>
2026-07-25 12:19:52 +0200

    feat(virtualisation/podman): persist .docker

--- modules/home/virtualisation/podman.nix
+++ modules/home/virtualisation/podman.nix
@@ -6,12 +6,16 @@
 }:
 let
   virtualisationCfg = osConfig._.virtualisation;
+  homeCfg = config.home;
   xdgCfg = config.xdg;
 in
 {
   config = lib.mkIf virtualisationCfg.podman.enable {
     _ = {
       nixos.impermanence.directories = {
+        "${homeCfg.homeDirectory}/.docker" = {
+          restic = false;
+        };
         "${xdgCfg.dataHome}/containers" = {
           restic = false;
         };
